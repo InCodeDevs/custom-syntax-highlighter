ef6b6247fab4e3585cf0d6252d1cf8ca4e8d2570
MCTzOCK <dev@mctzock.de>
2022-01-04 20:16:02 +0100

    Added TypeScript Declarations

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Highlights code blocks in a way you specify.
+ *
+ * @param  {Object} config    Allows the following keys:
+ *                              patterns:    [...] (The regex patterns used to parse)
+ *                              linenums:    true  (Turns on line numbers)
+ *                              selector:    'pre' (Defaults to 'pre code')
+ *                              preProcess:  fn    (Allows you to eff with the string after parsing)
+ *                              postProcess: fn    (Allows you to eff with the string after parsing)
+ *
+ * @return {undefined}
+ */
+declare function _exports(config: any): undefined;
+export = _exports;
+//# sourceMappingURL=index.d.ts.map
